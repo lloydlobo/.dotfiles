54c802dc7d817128da6c75df26909eecde26aa49
Lloyd Lobo <lloydlobo4@gmail.com>
2023-05-25 11:31:00 +0530

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,22 +5,22 @@ require("gg")
 -- print("hello from nvim/init.lua") -- debug.
 
 --
--- CHEATSHEET
+-- # CHEATSHEET
 --
 
--- :Ex -- Netrw Explorer.
--- % -- Create new file when in Netrw window.
--- :so -- *:so* *:source* *load-vim-script* :[range]so[urce] [file]
---     -- Runs |Ex| commands or Lua code (".lua" files) from [file], or current buffer if no [file].
---     -- Triggers the |SourcePre| autocommand.
--- :s/<from>/<to>/g -- Find & Replace, all words after cursor.
--- = -- Indents selection(s) or line.
+-- ## :Ex -- Netrw Explorer.
+-- ## % -- Create new file when in Netrw window.
+-- ## :so -- *:so* *:source* *load-vim-script* :[range]so[urce] [file]
+--       -- Runs |Ex| commands or Lua code (".lua" files) from [file], or current buffer if no [file].
+--       -- Triggers the |SourcePre| autocommand.
+-- ## :s/<from>/<to>/g -- Find & Replace, all words after cursor.
+-- ## = -- Indents selection(s) or line.
 
 ---
---- OS PREFERENCES
+--- # OS PREFERENCES
 ---
 
--- Set Dark Theme GTK for Linux
+-- ## Set Dark Theme GTK for Linux
 --
 -- Firefox uses the GTK theme settings. On sway set the setting gtk-application-prefer-dark-theme to 1 to get Firefox and GTK apps to use a dark theme by default.
 -- Edit (or create if it doesn't exist) the following file: ~/.config/gtk-3.0/settings.ini
@@ -31,9 +31,30 @@ require("gg")
 -- After that firefox and GTK apps were dark by default. It will likely require a restart of the application in the case of them already running.
 -- ```
 
+---
+--- # PROFILING
+---
+
+-- ## built-in profiling support: @ZyX https://stackoverflow.com/a/12216578
 --
--- GIT REFERENCES
+-- ```
+-- :profile start profile.log
+-- :profile func *
+-- :profile file *
+-- -- At thispoint do slow actions.
+-- :profile pause
+-- :noautocmd qall!
+-- ```
+-- ### Note:
+--
+-- - Won't get functions deleted before vim quits.
+-- - unlike quitting `:q or ZZ` `noautocmd` is not required, it just makes vim /neovim quit faster.
+-- - `:set more | verbose function {function_name}` shows location and content of functions.
 --
 
--- git log -S <filename>
--- git add -p -- patch (selects hunk interactively to stage)
+---
+--- # GIT REFERENCES
+---
+
+-- ## git log -S <filename>
+-- ## git add -p -- patch (selects hunk interactively to stage)
