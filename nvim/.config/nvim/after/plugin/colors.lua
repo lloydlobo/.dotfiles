6fd850ce47d6f59caf3d6fa52f826f15c23e9f96
Lloyd Lobo <lloydlobo4@gmail.com>
2023-05-31 01:10:12 -0400

    feat: add go binary to PATH environment variable

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -11,9 +11,10 @@ function ColorMyPencils(color)
     vim.cmd.colorscheme(color)        -- Sets colorscheme.
 
     -- `0` sets global space for every window. `Normal` is for vim.
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })         -- Sets transparency for background.
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })    -- Sets transparency for background floats too.
-    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#111111" }) -- Sets color for ColorColumn at 80 width. see lua/gg/sets.lua.
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })           -- Sets transparency for background.
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })      -- Sets transparency for background floats too.
+    --vim.api.nvim_set_hl(0, "ColorColumn", { bg = 13 }) -- Sets color for ColorColumn at 80 width. see lua/gg/sets.lua.
+    --vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#000000" }) -- Sets color for ColorColumn at 80 width. see lua/gg/sets.lua.
 end
 
 ColorMyPencils() -- Run `:lua ColorMyPencils()` from anywhere in neovim cmd.
--- profile/.profile
+++ profile/.profile
@@ -1,1 +1,2 @@
 . "$HOME/.cargo/env"
+export PATH=$PATH:/usr/local/go/bin
