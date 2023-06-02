42bbc892c9f98abc55e08678e6e6cc9872b42d3b
Lloyd Lobo <lloydlobo4@gmail.com>
2023-06-02 13:01:47 +0530

    fix(picom): patch overlapping transparent tabbed i3 window

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -16,7 +16,7 @@ xrandr -s $resolution &
 
 xmodmap ~/.Xmodmap &
 
-wal -a 90 -i ~/Pictures/wallpapers/wallhaven-dp19wl.jpg & # -i (Set transparency)
+wal -a 99 -i ~/Pictures/wallpapers/wallhaven-dp19wl.jpg & # -i (Set transparency)
 #wal -R &
 
 # `dunst` notification daemon.
