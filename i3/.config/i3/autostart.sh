605dad9b6978109fc60519b7d0547841b3d98659
Lloyd Lobo <lloydlobo4@gmail.com>
2023-06-02 15:44:09 +0530

    feat(i3): BREAKING CHANGE reorder autostart

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -1,34 +1,34 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
 # Start process.
+
+# Fit the VM Virtual Machine display to fullscreen
+resolution="1024x768"
+xrandr -s $resolution
 
 # Compositor
 killall picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+picom --config /home/lloyd/.config/picom/picom.conf --vsync -b # `picom` to reduce screen tearing. with `-b` daemonize flag. for kernel blur to work.
 #https://bbs.archlinux.org/viewtopic.php?id=268883
 #GLX backend is broken because that config doesn't expand the module path properly. Remove that xorg.conf and reboot. If it still doesn't work, enable early KMS: https://wiki.archlinux.org/title/NVIDIA … de_setting
-picom --config /home/lloyd/.config/picom/picom.conf --vsync -b & # `picom` to reduce screen tearing. with `-b` daemonize flag. for kernel blur to work.
 
-# Fit the VM Virtual Machine display to fullscreen
+# Open predefined windows. @source echasnovski/dotfiles.
+# Permanent windows (usually should stay opened).
+# TODO: close i3-sensible-terminal with btm, and open it.
+i3-msg 'workspace "10:monitor"; layout splith'
+sleep 1
+i3-sensible-terminal -e btm
+sleep 1
+i3-msg 'workspace "1";'
 
-resolution="1024x768"
-xrandr -s $resolution &
+$HOME/.config/polybar/launch.sh
 
+xmodmap ~/.Xmodmap
 
-xmodmap ~/.Xmodmap &
+#dunst -config ~/.config/dunst/dunstrc &
 
 #wal -a 99 --saturate 0.25 -i ~/Pictures/wallpapers/wallhaven-dp19wl.jpg & # -i (Set transparency)
-wal -a 99 --saturate 0.15 -i ~/Pictures/wallpapers/wallhaven-dp19wl.jpg & # -i (Set transparency)
-#wal -R &
-
-# `dunst` notification daemon.
-dunst -config ~/.config/dunst/dunstrc &
-
-# Open predefined windows.
-# Permanent windows (usually should stay opened).
-# @source echasnovski/dotfiles.
-
-$HOME/.config/polybar/launch.sh &
-i3-msg 'workspace "10:monitor"; layout splith' #sleep 1
-i3-sensible-terminal -e btm #sleep 1
-
+# -i (Set transparency)
+#wal -a 99 --saturate 0.20 -i ~/Pictures/wallpapers/wallhaven-dp19wl.jpg &
+wal -R
