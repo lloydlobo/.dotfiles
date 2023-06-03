e4472e378cc3855070ca5c7df1af600f4ed3e864
Lloyd Lobo <lloydlobo4@gmail.com>
2023-06-03 16:04:02 +0530

    ci(.github): fix trailing comma JSON benchmark error

--- .github/scripts/benchmarks.sh
+++ .github/scripts/benchmarks.sh
@@ -11,21 +11,36 @@ set -eu
 # zsh shell runs in login interactive mode, loading ~/.zshrc and immediately exits.
 # Loop output is redirected to /tmp/zsh-load-time.txt , discarding errors.
 { for i in $(seq 1 10); do
-    /usr/bin/time --format="%e" -o /dev/stdout zsh -li --rcs ~/.zshrc -c exit;
+    /usr/bin/time --format="%e" -o /dev/stdout zsh -li --rcs ~/.dotfiles/zsh/.zshrc -c exit;
   done;
 } >/tmp/zsh-load-time.txt 2>/dev/null
 
 ZSH_LOAD_TIME=$(awk '{ total += $1 } END { print total/NR }' /tmp/zsh-load-time.txt)
 
 # HACK: removes ansi escape sequences on the last line.
-head -n -1 /tmp/zsh-load-time.txt | tee /tmp/zsh-load-time.txt>/dev/null
+# NOTE: `2>/dev/null` Alone prints to stdout, so we use `>/dev/null` before that.
+head -n -1 /tmp/zsh-load-time.txt | tee /tmp/zsh-load-time.txt>/dev/null 2>/dev/null
 
+# Output a JSON-like structure using a here document redirection (<<EOJ ... EOJ).
+# NOTE: Do not add trailing comma to the last json line ("value"...)
 cat <<EOJ
 [
   {
       "name": "zsh load time",
       "unit": "Second",
-      "value": ${ZSH_LOAD_TIME},
+      "value": ${ZSH_LOAD_TIME}
   }
 ]
 EOJ
+
+# In shell scripting, the `>/dev/null` and `2>/dev/null` constructs are
+# used for redirecting output and error streams respectively to the
+# null device (`/dev/null`).
+#
+# `>/dev/null` redirects the standard output, and `2>/dev/null` redirects
+# the standard error. By combining these redirections, you can
+# effectively discard both output and error messages.
+#
+# For example, `command >/dev/null 2>/dev/null` redirects both stdout
+# and stderr to `/dev/null`, effectively silencing any output or error
+# from the command.
