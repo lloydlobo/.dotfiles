f2ebfddede2a60ea9789c0ca6354b35a48e9281c
Lloyd Lobo <lloydlobo4@gmail.com>
2023-06-03 12:40:34 +0530

    style(wezterm): improve appearance

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,8 @@
+-- Awesome dotfile: https://github.com/yutkat/dotfiles/blob/main/.config/wezterm/wezterm.lua
+
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local scheme = wezterm.get_builtin_color_schemes()["Batman"]
 
 -- This table will hold the configuration.
 local config = {}
@@ -15,25 +18,59 @@ end
 config.window_frame = {
   font = wezterm.font { family = 'JetBrainsMono Nerd Font', weight = 'Bold' },
   font_size = 8.0,
-  active_titlebar_bg = '#111111',
+  active_titlebar_bg = '#030303',
   inactive_titlebar_bg = '#111111',
 }
 
 config.window_background_opacity = 0.80
-config.text_background_opacity = 0.5 -- The text_background_opacity setting specifies the alpha channel value to use for the background color of cells other than the default background color.
+config.text_background_opacity = 0.61 -- The text_background_opacity setting specifies the alpha channel value to use for the background color of cells other than the default background color.
 config.color_scheme = 'Gruvbox Red'
+--config.color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" }
 config.font = wezterm.font { family = 'JetBrainsMono Nerd Font', weight = 'Regular' }
-config.font_size = 9.0
+config.font_size = 8.5
+--config.cell_width = 1.1
+--config.line_height = 1.1
+config.check_for_updates = true
+config.use_ime = true
+--config.cursor_blink_ease_in = "Constant"
+--config.cursor_blink_ease_out = "Constant"
+--config.cursor_blink_rate = 0
+config.hide_tab_bar_if_only_one_tab = false
+config.adjust_window_size_when_changing_font_size = false
+config.selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%"
+config.window_padding = {
+  top = 0,
+  right = 0,
+  bottom = 0,
+  left = 0,
+}
+config.use_fancy_tab_bar = false
 
 config.colors = {
   tab_bar = {
-    inactive_tab_edge = '#575757',
-    --inactive_tab = {
-    --  bg_color = '#2a2a2a',
-    --  fg_color = '#aaaaaa',
-    --}
+    background = scheme.background,
+    new_tab = { bg_color = "#2e3440", fg_color = scheme.ansi[8], intensity = "Bold" },
+    new_tab_hover = { bg_color = scheme.ansi[1], fg_color = scheme.brights[8], intensity = "Bold" },
+    -- format-tab-title
+    -- active_tab = { bg_color = "#121212", fg_color = "#FCE8C3" },
+    -- inactive_tab = { bg_color = scheme.background, fg_color = "#FCE8C3" },
+    -- inactive_tab_hover = { bg_color = scheme.ansi[1], fg_color = "#FCE8C3" },
   },
 }
+
+config.exit_behavior = "CloseOnCleanExit"
+config.tab_bar_at_bottom = false
+config.window_close_confirmation = "AlwaysPrompt"
+config.disable_default_key_bindings = false
+
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+table.insert(config.hyperlink_rules, {
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = "https://github.com/$1/$3",
+})
+
+--config.enable_csi_u_key_encoding = true
+--config.leader = { key = "Space", mods = "CTRL|SHIFT" }
 
 config.keys = {
   {
