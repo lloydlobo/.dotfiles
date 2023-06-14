ea2aa37e48194fe9012d406f785fb09f5a7b6b6e
Lloyd Lobo <lloydlobo4@gmail.com>
2023-06-14 10:32:38 +0530

    chore(i3): use wezterm as default terminal

--- i3/.config/i3/autostart.sh
+++ i3/.config/i3/autostart.sh
@@ -10,6 +10,7 @@ xrandr -s $resolution
 killall picom
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 picom --config /home/lloyd/.config/picom/picom.conf --vsync -b # `picom` to reduce screen tearing. with `-b` daemonize flag. for kernel blur to work.
+
 #https://bbs.archlinux.org/viewtopic.php?id=268883
 #GLX backend is broken because that config doesn't expand the module path properly. Remove that xorg.conf and reboot. If it still doesn't work, enable early KMS: https://wiki.archlinux.org/title/NVIDIA … de_setting
 
@@ -24,14 +25,13 @@ picom --config /home/lloyd/.config/picom/picom.conf --vsync -b # `picom` to redu
 
 "$HOME"/.config/polybar/launch.sh
 
-# xmodmap ~/.Xmodmap
+# xmodmap ~/.Xmodmap  # Disabled custom key remaps, now using `-option`
 setxkbmap # activate dvorak.
 setxkbmap -option ctrl:nocaps # Remap Capslock as Control.
 
-#dunst -config ~/.config/dunst/dunstrc &
+dunst -config ~/.config/dunst/dunstrc &
 
 # -a (Sets transparency)
-
 #wal -a 99 --saturate 0.20 -i ~/Pictures/wallpapers/alena-aenami-15step-AEE71.png &
-wal -a 99 --saturate 0.30 -i ~/Pictures/wallpapers/wallhaven-dp19wl.jpg &
+wal -a 99 --saturate 0.15 -i ~/Pictures/wallpapers/wallhaven-dp19wl.jpg &
 #wal -R
